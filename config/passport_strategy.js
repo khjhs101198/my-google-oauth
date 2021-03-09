5c128a6634857e63b7c81d878898183565388fd3
khjhs101198 <khjhs101198@gmail.com>
2021-03-09 14:56:52 +0800

    fix callbackURL again

--- config/passport_strategy.js
+++ config/passport_strategy.js
@@ -4,7 +4,7 @@ const GoogleStrategy = require("passport-google-oauth20").Strategy;
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "auth/google/callback"
+    callbackURL: "/auth/google/callback"
 }, function(accessToken, refreshToken, profile, cb) {
 
 }));
